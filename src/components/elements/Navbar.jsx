35557359391d2f1a98d48a13b159f0f2631b3af4
rierangel <angelriera1796@gmail.com>
2022-08-25 15:43:39 -0400

    navdrop

--- src/components/elements/Navbar.jsx
+++ src/components/elements/Navbar.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
+import Link from 'next/link'
+import styles from './styles.module.scss'
+import NavDrop from './NavDrop'
+import Socials from './Socials'
 
 export default function Navbar() {
+
+    const nosotros = [
+        {
+            name: "inicio",
+            link: "/"
+        },
+        {
+            name: "Marco Normativo",
+            link: "/"
+        },
+
+    ]
+
     return (
-        <nav>
-            
-            Navbar
+        <nav className={styles.navbar}>
+            <div className="container">
+                <div className={styles.top}>
+                    <div className={styles.img}>
+                        <img src="/img/logo.png" alt="" />
+                    </div>
+
+                    <Socials />
+                </div>
+                <div className={styles.bottom}>
+
+
+                    <ul className='flex space-x-[40px]'>
+                        <li >
+                            <Link className='mb-auto' href="/">
+                                <a className=''>inicio</a>
+                            </Link>
+                        </li>
+
+                        <NavDrop/>
+
+
+                    </ul>
+                </div>
+
+            </div>
         </nav>
     )
 }
