49e10f8cb255afef8e98af27dc7ee1fa1e4cfdef
ragnarok <angelriera1796@gmail.com>
2022-08-31 16:00:41 -0400

    content rendered

--- src/components/noticia/Content.jsx
+++ src/components/noticia/Content.jsx
@@ -6,47 +6,56 @@ export default function ContentRef({ data }) {
     const contentRef = useRef()
     const [galeryImgs, setGaleryImgs] = useState([])
 
-    if(galeryImgs.current){
-        for (let index = 0; index < contentRef.current.children.length; index++) {
-            const element = contentRef.current.children[index];
-            if (element.tagName == "FIGURE") {
-                if (element.children[0]) {
-                    if (element.children[0].tagName == "IMG") {
-                        let arry = galeryImgs
-                        arry.push(element)
-                        setGaleryImgs(arry)
-                    } else {
-                        for (let ifigure = 0; ifigure < element.children.length; ifigure++) {
-                            let subfigure = element.children[ifigure];
-                            let arry = galeryImgs
-                            arry.push(subfigure)
-                            setGaleryImgs(arry)
+    const [content, setContent] = useState(null)
+
+    useEffect(() => {
+        setContent(null)
+        setGaleryImgs([])
+
+        setContent(<p className={styles.article} ref={contentRef} dangerouslySetInnerHTML={{ __html: data }} />)
+    }, [data])
+
+    useEffect(() => {
+        if (content) {
+            if (content.ref.current) {
+                for (let index = 0; index < content.ref.current.children.length; index++) {
+                    const element = content.ref.current.children[index];
+                    if (element.children[0]) {
+                        if (element.children[0].tagName == "IMG") {
+                            const newArry = galeryImgs
+                            newArry.push(element)
+                            setGaleryImgs(newArry)
+                        } else if (element.children[0].tagName == "FIGURE") { // esto es una galeria                        
+                            for (let ifigure = 0; ifigure < element.children.length; ifigure++) {
+                                const subfigure = element.children[ifigure];
+                                const newArry = galeryImgs
+                                newArry.push(subfigure)
+                                setGaleryImgs(newArry)
+                            }
                         }
                     }
                 }
-            }
-        }
-        const imgs = [...galeryImgs];
-        const result = imgs.filter((item, index) => {
-            return imgs.indexOf(item) === index;
-        })
-        setGaleryImgs(result)
-    }
+                console.log(galeryImgs)
+                // for (let index = 0; index < content.ref.current.children.length; index++) {
+                //     const element = content.ref.current.children[index];
+                //     if (element.tagName == "FIGURE") {
+                //         console.log("asda")
+                //         content.ref.current.removeChild(element)
+                //     }
+                // }
 
-    if(galeryImgs.length > 0){
-        for (let index = 0; index < contentRef.current.children.length; index++) {
-            const element = contentRef.current.children[index];
-            if (element.tagName == "FIGURE") {
-                contentRef.current.removeChild(element)
             }
+
         }
-    }
+    }, [content])
+
 
     return (
         <>
-            {galeryImgs.length > 0 && <Carrusel data={galeryImgs} />}
+            {false && galeryImgs.length > 0 && <Carrusel data={galeryImgs} />}
 
-            <p className={styles.article} ref={contentRef} dangerouslySetInnerHTML={{ __html: data }} />
+            <button onClick={()=>setGaleryImgs([])}>recet</button>
+            {content}
         </>
     )
 }
