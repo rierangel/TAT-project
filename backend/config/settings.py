cdb7bff3e04340e6436f5a96cfb7702a9c8fd0da
ragnarok <angelriera1796@gmail.com>
2022-11-13 14:55:32 -0400

    secret key

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -34,7 +34,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))  # add this
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "*"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
