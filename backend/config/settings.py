ba21d0139a4a8d34edb3a03b3d5266cf0040f8b6
ragnarok <angelriera1796@gmail.com>
2022-11-16 12:45:05 -0400

    storage s3

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -45,7 +45,7 @@ if DEBUG:
 CSRF_TRUSTED_ORIGINS = [env("BACK_URL")]
 CORS_ALLOWED_ORIGINS = [env("FRONT_URL"),]
 
-print(CORS_ALLOWED_ORIGINS,DEBUG, ALLOWED_HOSTS )
+# print(CORS_ALLOWED_ORIGINS,DEBUG, ALLOWED_HOSTS )
 
 # Application definition
 sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
@@ -57,7 +57,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+
     # packages
+    'storages',
     'rest_framework',
     'django_summernote',
 
@@ -178,13 +180,34 @@ STATIC_URL = STATIC_HOST + "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
+
+AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400',}
+AWS_LOCATION = 'static'
+
+AWS_S3_FILE_OVERWRITE = True
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
+
+
+
+
+# AWS_BUCKET_NAME = ""
+# AWS_ACCESS_KEY_ID = ""
+# AWS_SECRET_ACCESS_KEY = ""
+# AWS_REGION = ""
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
