c6e2428da8dc1148f3b6e486bd93faa7b6f33fe0
ragnarok <angelriera1796@gmail.com>
2022-11-16 14:41:20 -0400

    frontend actualizacion

--- frontend/src/components/Layer/ImgBack.jsx
+++ frontend/src/components/Layer/ImgBack.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 export default function ImgBack({src, alt, className}) {
 
   return (
-    <img src={`${process.env.NEXT_PUBLIC_URL_BACKEND}${src}`} alt={alt} className={className} />
+    // <img src={`${process.env.NEXT_PUBLIC_URL_BACKEND}${src}`} alt={alt} className={className} />
+    <img src={src} alt={alt} className={className} />
   )
 }
